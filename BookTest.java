217a0e5a49665374bfcf827939099f9c6e031226
Luke Stein <stein.luke404@gmail.com>
2015-10-05 18:32:14 +1100

    added BookTest.java

---
+++ BookTest.java
@@ -0,0 +1,31 @@
+import static org.mockito.Mockito.*;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+
+import library.entities.Book;
+import library.interfaces.entities.ILoan;
+import library.interfaces.entities.EBookState;
+
+public class BookTest {
+	private ILoan _loan;
+	private Book book;
+
+	@Before
+	public void setUp() {
+		_loan = mock(ILoan.class);
+		book = new Book("Test author", "Test title", "Test callnumber", 123456);
+	}
+
+	@Test
+	public void testBorrow() {
+		book.borrow(_loan);
+		assertEquals(book.getLoan(), _loan);
+		assertEquals(book.getState(), EBookState.ON_LOAN);
+	}
+
+	public static void main(String[] args) {
+		org.junit.runner.JUnitCore.main("BookTest");
+	} 
+}
